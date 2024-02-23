906015b90965786ef50bc5327e262693dce61e8b
ankur.singh <ankursingh20201997@gmail.com>
2024-02-23 14:41:15 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1 , col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png", )
+
+with col2:
+    st.title("Ardit sulce")
+    content = """Hello! My name is Vicky, and I'm passionate about sports. I have a background in Football and played in FC barcelona as a mid striker"""
+    st.info(content)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
